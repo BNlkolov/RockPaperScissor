31bb6cde2a31e2d323fbe20599680ac4aae44ca4
Borislav Nikolov <borislavn441@gmail.com>
2023-09-19 22:51:09 +0300

    .

---
+++ src/RockPaperScissor.java
@@ -0,0 +1,5 @@
+public class RockPaperScissor {
+
+    // all  the  choices that a computer can choose
+    private static final String[] computerChoice = {"Rock", "Paper", "Scissor"};
+}
--- src/RockPaperScissorGUI.java
+++ src/RockPaperScissorGUI.java
@@ -1,2 +1,140 @@
-package PACKAGE_NAME;public class RockPaperScissorGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+//FRONTEND
+public class RockPaperScissorGUI extends JFrame {
+
+    //player buttons
+    JButton rockButton, paperButton, scissorButton;
+
+    //will display the choice of the computer;
+    JLabel computerChoice;
+
+    public RockPaperScissorGUI() {
+
+        //invoke JFrame constructor and add title to the GUI
+        super("Rock Paper Scissor");
+
+        //set the size of the GUI
+        setSize(450, 574);
+
+        //set layout to disable layout management,so we can use absolute positioning for the elements
+        setLayout(null);
+
+        //terminate the java virtual machine when closing the GUI
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // load GUI in the center of the screen every time we run application
+        setLocationRelativeTo(null);
+
+
+        //add GUI components
+        addGuiComponents();
+        //add Player components
+        //addPlayerComponents();
+
+
+    }
+
+
+    private void addGuiComponents() {
+
+        //create computer score label
+        JLabel computerScoreLabel = new JLabel("Computer: 0");
+
+        // set x,y coordinates nad width/height values
+        computerScoreLabel.setBounds(0, 43, 450, 30);
+
+        //set the font to have a font family of dialog,font-weight of bold,and a font-size of 26
+        computerScoreLabel.setFont(new Font("Dialog", Font.BOLD, 26));
+
+        //place the text in the center
+        computerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //add to GUI
+        add(computerScoreLabel);
+
+        //create computer choice
+        computerChoice = new JLabel(("?"));
+        computerChoice.setBounds(175, 118, 98, 81);
+        computerChoice.setFont(new Font("Dialog", Font.PLAIN, 18));
+        computerChoice.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //create a black border around
+        computerChoice.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        add(computerChoice);
+
+        //create player score label
+        JLabel playerScoreLabel = new JLabel("Player: 0");
+
+        //set x,y coordinates width/height values
+        playerScoreLabel.setBounds(0, 317, 450, 30);
+
+        //set the font to have a font family of dialog,font-weight of bold,and a font-size of 26
+        playerScoreLabel.setFont(new Font("Dialog", Font.BOLD, 26));
+
+        //place the text in the center
+        playerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //add to GUI
+        add(playerScoreLabel);
+
+        //player buttons
+
+
+        //rock button
+        rockButton = new JButton("Rock");
+        rockButton.setBounds(40, 387, 105, 81);
+        rockButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+        add(rockButton);
+
+        //paper button
+        paperButton = new JButton("Paper");
+        paperButton.setBounds(165, 387, 105, 81);
+        paperButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+        add(paperButton);
+
+        //scissor button
+        scissorButton = new JButton("Scissor");
+        scissorButton.setBounds(290, 387, 105, 81);
+        scissorButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+        add(scissorButton);
+
+
+    }
+
+    //displays a message dialog which will show the winner and a try again button to play again
+    private void showDialog(String message) {
+        JDialog resultDialog = new JDialog(this, "Result", true);
+        resultDialog.setSize(227, 124);
+        resultDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        resultDialog.setResizable(false);
+
+        //message label
+        JLabel resultLabel = new JLabel(message);
+        resultLabel.setFont(new Font("Dialog", Font.BOLD, 18));
+        resultLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        resultLabel.add(resultLabel,BorderLayout.CENTER);
+
+        //try again button
+        JButton tryAgainButton = new JButton("Try Again?");
+        tryAgainButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //reset computer choice
+                    computerChoice.setText("?");
+
+                    //close the dialog box
+                resultDialog.dispose();
+            }
+        });
+        resultDialog.add(tryAgainButton,BorderLayout.SOUTH);
+
+        resultDialog.setLocationRelativeTo(this);
+        resultDialog.setVisible(true);
+    }
 }
+
